75824ee9306c211e278209dbfba26838c1495d82
n-bhasin <nkbhasin28@gmail.com>
2019-11-16 19:33:38 -0500

    Updates

---
+++ precache-manifest.e410208dd59f676ddaa35e244f283c93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36f33427c28742661881f556b2d2772e",
+    "url": "/first-app/index.html"
+  },
+  {
+    "revision": "f27414b1d599807e2559",
+    "url": "/first-app/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "839a3b45e420c4c8b51f",
+    "url": "/first-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f27414b1d599807e2559",
+    "url": "/first-app/static/js/2.4cd98d95.chunk.js"
+  },
+  {
+    "revision": "839a3b45e420c4c8b51f",
+    "url": "/first-app/static/js/main.5e8c2d35.chunk.js"
+  },
+  {
+    "revision": "53ea71c3d7eaf0a197e8",
+    "url": "/first-app/static/js/runtime-main.21bb9908.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/n-bhasin/first-react-app/precache-manifest.d27f7d9893ac0cec87dfd1e8062c9eb4.js"
+  "/first-app/precache-manifest.e410208dd59f676ddaa35e244f283c93.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/n-bhasin/first-react-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/first-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
